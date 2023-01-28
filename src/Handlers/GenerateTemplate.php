54976fdbfb563bbb3dc502d01262d7ad1cecb676
Marcel Hernandez <e6990620@gmail.com>
2023-01-28 12:17:19 +0100

    parse input

--- src/Handlers/GenerateTemplate.php
+++ src/Handlers/GenerateTemplate.php
@@ -7,6 +7,7 @@ namespace AutoNode\Handlers;
 use AutoNode\Domain\Feature\Basics;
 use AutoNode\Domain\CloudInitTemplate;
 use AutoNode\Domain\User;
+use AutoNode\Handlers\GenerateTemplate\Input;
 use Nyholm\Psr7\Response;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
@@ -17,11 +18,13 @@ final class GenerateTemplate implements RequestHandlerInterface
 {
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        $adminUser = new User('satoshi', 'Admin user', true);
+        $input = Input::fromForm($request->getParsedBody());
+
+        $adminUser = new User($input->admin, 'Admin user', true);
         $adminUser->addAuthorizedKey('ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKwg+bJZ7RVMbwslBzMlN2+Hfe13fCD8u2IxAZZoHeQ5 root@d2db98313a74');
         $adminUser->addImportId('gh', '1ma');
 
-        $template = new CloudInitTemplate('autonode', 'en_US.UTF-8', $adminUser);
+        $template = new CloudInitTemplate($input->hostname, 'en_US.UTF-8', $adminUser);
         $template->add(new Basics());
 
         return new Response(
---
+++ src/Handlers/GenerateTemplate/Input.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AutoNode\Handlers\GenerateTemplate;
+
+final class Input
+{
+    public string $admin;
+    public string $hostname;
+    public array $sshKeys;
+    public bool $withTor;
+    public bool $withNginx;
+    public bool $withSparrow;
+    public bool $withWireGuard;
+    public array $bitcoinCore;
+    public array $electrs;
+    public array $btcRpcExplorer;
+    public array $coreLightning;
+    public array $rideTheLightning;
+
+    private function __construct()
+    {
+    }
+
+    public static function fromForm(array $form): Input
+    {
+        $input = new Input();
+
+        $input->admin = $form['admin-username'];
+        $input->hostname = $form['hostname'];
+        $input->withTor = $form['with-tor-services'] === 'on';
+        $input->withNginx = $form['with-nginx'] === 'on';
+        $input->withSparrow = $form['with-sparrow'] === 'on';
+        $input->withWireGuard = $form['with-wireguard'] === 'on';
+
+        $input->bitcoinCore = [];
+        if ($form['bitcoin-core-version'] !== 'none') {
+            $input->bitcoinCore['version'] = $form['bitcoin-core-version'];
+            $input->bitcoinCore['dbcache'] = (int) $form['bitcoin-core-dbcache'];
+        }
+
+        $input->electrs = [];
+        if ($form['electrs-version'] !== 'none') {
+            $input->electrs['version'] = $form['electrs-version'];
+        }
+
+        $input->btcRpcExplorer = [];
+        if ($form['btcexp-version'] !== 'none') {
+            $input->btcRpcExplorer['version'] = $form['btcexp-version'];
+            $input->btcRpcExplorer['currency'] = $form['btcexp-currency'];
+            $input->btcRpcExplorer['theme'] = $form['btcexp-theme'];
+        }
+
+        $input->coreLightning = [];
+        if ($form['cln-version'] !== 'none') {
+            $input->coreLightning['cln'] = $form['electrs-version'];
+        }
+
+        $input->rideTheLightning = [];
+        if ($form['rtl-version'] !== 'none') {
+            $input->rideTheLightning['version'] = $form['rtl-version'];
+        }
+
+        return $input;
+    }
+}
